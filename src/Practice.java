f2b1b11925cfb8722105d35ecfe58c1db3dd479a
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-04-08 07:37:43 -0700

    biggest number

--- src/Practice.java
+++ src/Practice.java
@@ -83,7 +83,18 @@ public class Practice {
      * @throws IllegalArgumentException if head is null
      */
     public static int biggestNumber(ListNode<Integer> head) {
-        return 0;
+        if(head == null) {
+            throw new IllegalArgumentException();
+        }
+        ListNode<Integer> current = head;
+        int biggestNum = -Integer.MIN_VALUE;
+        while(current != null) {
+            if(current.data > biggestNum) {
+                biggestNum = current.data;
+            }
+            current = current.next;
+        }
+        return biggestNum;
     }
 
     /**
